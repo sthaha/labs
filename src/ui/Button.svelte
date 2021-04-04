d2abc8d8bede3a63a3ac15be3b4778a207eaad7e
Sunil Thaha <3005132+sthaha@users.noreply.github.com>
2021-04-04 20:06:59 +1000

    UI: add button and a Toggle UI component

---
+++ src/ui/Button.svelte
@@ -0,0 +1,12 @@
+<button
+  class="rounded-xl shadow bg-green-600 text-center text-gray-100"
+  class:min-w-full={ $$slots.contents }
+  class:h-10={ $$slots.contents }
+  class:px-2={ $$slots.contents }
+  on:click
+>
+  <slot name="contents">
+    <div class="w-4 h-4"> </div>
+  </slot>
+</button>
+
---
+++ src/ui/Toggle.svelte
@@ -0,0 +1,39 @@
+<script>
+  export let active = true
+  export let invert = false
+
+  import { createEventDispatcher } from 'svelte';
+
+	const dispatch = createEventDispatcher();
+
+  const toggle = () => {
+    active  = !active
+    dispatch('toggled', {active});
+  }
+</script>
+
+<button
+  class="rounded-xl shadow text-center text-gray-100"
+  class:min-w-full={ $$slots.active || $$slots.inactive }
+  class:h-10={ $$slots.active || $$slots.inactive }
+  class:px-2={ $$slots.active || $$slots.inactive }
+  class:bg-green-600={!invert && active || invert && !active}
+  class:bg-gray-600={!invert && !active || invert && active}
+  on:click={toggle}>
+  {#if false}<slot></slot>{/if}
+
+  {#if active}
+    {#if $$slots.active}
+      <slot name="active"></slot>
+    {:else}
+      <div class="w-4 h-4"> </div>
+    {/if}
+  {:else}
+
+    {#if $$slots.inactive }
+      <slot name="inactive"></slot>
+    {:else}
+      <div class="w-4 h-4"> </div>
+    {/if}
+  {/if}
+</button>
