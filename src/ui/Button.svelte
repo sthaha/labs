968cf3162126f370a395a7403d75c2be382c4fba
Sunil Thaha <3005132+sthaha@users.noreply.github.com>
2021-04-13 01:17:17 +1000

    Button: allow bg color to be set

--- src/ui/Button.svelte
+++ src/ui/Button.svelte
@@ -1,9 +1,10 @@
 <button
-  class={ " rounded-xl shadow text-center text-gray-100 " + $$props.class || "" }
-  class:bg-green-600={ !$$props.class }
+  class={ " rounded-xl shadow text-center text-gray-100 hover:shadow-lg transition " + $$props.class || "" }
+  class:bg-green-600={ !$$props.class || !$$props.class.includes("bg-") }
   class:min-w-full={ $$slots.contents }
   class:h-10={ $$slots.contents }
   class:px-2={ $$slots.contents }
+
   on:click
 >
   <slot name="contents">
