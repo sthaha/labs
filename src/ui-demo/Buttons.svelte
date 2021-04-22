9d6c14acef06d6906faabf19236f40f70daabbaa
Sunil Thaha <3005132+sthaha@users.noreply.github.com>
2021-04-22 23:28:03 +1000

    Button now has a enabled prop

--- src/ui-demo/Buttons.svelte
+++ src/ui-demo/Buttons.svelte
@@ -9,6 +9,14 @@
     <Button > <span slot="contents"> Button expands to size of Text </span> </Button>
   </div>
 
+  <div class="flex gap-4 bg-gray-200 items-center">
+    <Button enabled={false} />
+    <Button  />
+
+    <Button enabled={false} > <span slot="contents"> Text </span> </Button>
+    <Button > <span slot="contents"> Text </span> </Button>
+  </div>
+
   <div class="flex flex-row items-center gap-2 bg-red-200 p-2" >
     <Button />
     <Button >
--- src/ui/Button.svelte
+++ src/ui/Button.svelte
@@ -1,15 +1,33 @@
+<script>
+import { createEventDispatcher } from "svelte"
+
+export let enabled = true
+
+const dispatch=createEventDispatcher()
+const handleClick = (e) => {
+  if (!enabled){
+    return
+  }
+  dispatch('click', e)
+}
+
+</script>
+
 {#if $$slots.contents }
+
 
   <button
     class={
       "inline-block px-4 py-2 rounded-2xl shadow " +
-      "text-center text-gray-200 " +
+      "text-center " +
       "hover:shadow-lg transition " +
       ($$props.class || "")
     }
-
-    class:bg-green-600={ !$$props.class || !$$props.class.includes("bg-") }
-    on:click
+    class:text-gray-200={ enabled }
+    class:text-gray-500={ !enabled }
+    class:bg-green-600={ ( !$$props.class || !$$props.class.includes("bg-") ) && enabled }
+    class:bg-gray-700={ !enabled }
+    on:click={handleClick}
   >
     <slot name="contents">
     </slot>
@@ -22,8 +40,9 @@
       "inline-block shadow h-4 w-4 md:h-8 md:w-8 rounded-full " +
       " hover:shadow-lg transition " + ($$props.class || "")
     }
-    class:bg-green-600={ !$$props.class || !$$props.class.includes("bg-") }
-    on:click
+    class:bg-green-600={ (!$$props.class || !$$props.class.includes("bg-")) && enabled }
+    class:bg-gray-600={ !enabled }
+    on:click={handleClick}
   >
   </button>
 
