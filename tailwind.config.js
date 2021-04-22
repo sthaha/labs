702235358986ea15be4e4a90e667139b64d380b4
Sunil Thaha <3005132+sthaha@users.noreply.github.com>
2021-04-22 23:27:34 +1000

    Tailwind: h-screen-0.75 ...

--- tailwind.config.js
+++ tailwind.config.js
@@ -22,10 +22,20 @@ module.exports = {
   },
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      height: theme => ({
+        "screen-0.75": "75vh",
+        "screen/2": "50vh",
+        "screen/3": "calc(100vh / 3)",
+        "screen/4": "calc(100vh / 4)",
+        "screen/5": "calc(100vh / 5)",
+      }),
+    },
   },
   variants: {
-    extend: {},
+    extend: {
+      display: ["responsive", "hover", "focus"],
+    },
   },
   plugins: [],
 }
