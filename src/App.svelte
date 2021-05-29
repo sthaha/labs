a5b0a0def91a961983ea28f6056e8b87ebf9f8e9
Sunil Thaha <3005132+sthaha@users.noreply.github.com>
2021-05-29 15:50:03 +1000

    App: keep routes and links in same file

--- src/App.svelte
+++ src/App.svelte
@@ -15,11 +15,23 @@ import FFT from "./math/FourierTransform.svelte";
 
 import SvelteIndex from "./svelte/Index.svelte";
 import UIDemo from "./ui-demo/Index.svelte";
+import IndexTable from "./ui/IndexTable.svelte";
 
-import LinksTable from './LinksTable.svelte';
 
 export let url = "";
+
+let entries = [
+  {link: "apps/stopwatch", summary: "Stop Watch", description: "Work in progress Stop watch for competetive test"},
+  {link: "beyond/spirograph", summary: "Spirograph", description: "Spirograph creates beautiful patterns"},
+  {link: "beyond/parametric-eq", summary: "Line Animation", description: "Line animations using parametric equations"},
+  {link: "math/fs", summary: "Fourier Series ", description: "Fourier series using spinning discs"},
+  /* {link: "math/fft", summary: "Discrete Fourier Transform ", description: ""}, */
+  {link: "svelte", summary: "Svelte experiments", description: "Tryout things ing svelte"},
+  {link: "ui", summary: "UI components", description: "UI components used in the lab"},
+]
+
 </script>
+
 
 <Router url="{url}">
   <Nav/>
@@ -37,7 +49,9 @@ export let url = "";
       <Route path="ui/*" component="{UIDemo}" />
       <Route path="svelte/*" component="{SvelteIndex}" />
 
-      <Route path="/" component="{LinksTable}" />
+      <Route path="/">
+        <IndexTable {entries} />
+      </Route >
 
     </div>
   </div>
